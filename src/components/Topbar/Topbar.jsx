553b4545be6942f7778e4650194938ead2bd350e
alaric29 <alaric919@gmail.com>
2021-07-20 11:09:46 +0200

    added hamburger menu

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,14 +1,30 @@
 import "./topbar.scss";
+import {Person, Mail} from '@material-ui/icons';
+
 
 const Topbar = props => {
   return (
-    <div className="topbar">
+    <div className="topbar active">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">clever.</a>
+          <div className="itemContainer">
+          <Person className="icon"/>
+          <span>+604 250 5000</span>
+          </div>
+          <div className="itemContainer">
+          <Mail className="icon"/>
+          <span>vulpine@clever.io</span>
+          </div>
         </div>
 
-        <div className="right"></div>
+        <div className="right">
+        <div className="hamburger">
+          <span className="line-1"></span>
+          <span className="line-2"></span>
+          <span className="line-3"></span>
+        </div>
+        </div>
       </div>
     </div>
   )
