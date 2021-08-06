90a32251aafc9cbf83fac7b342976765efe7d0b1
alaric29 <alaric919@gmail.com>
2021-08-06 10:39:57 +0200

    add intro section and functionality

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,7 +3,22 @@ import "./intro.scss";
 const Intro = props => {
   return (
     <div id="intro" className="intro">
+        <div className="left">
+          <div className="imgContainer">
+            <img src="assets/happyMan1.png" alt="" />
+          </div>
+        </div>
+        <div className="right">
+          <div className="wrapper">
+            <h2>Hi There. I'm </h2>
+            <h1>Alaric Ve</h1>
+            <h3>Freelance <span>Developer</span></h3>
 
+          </div>
+          <a href="#portfolio">
+            <img src="assets/downArrow.png" alt=""/>
+          </a>
+        </div>
     </div>
   )
 };
