3f1a4bc21fa7b4aad8a4a0bd888bc3f7ea10cac0
alaric29 <alaric919@gmail.com>
2021-08-10 14:51:59 +0200

    added portfolio section and functionality with dummy data

---
+++ src/components/PortfolioList/PortfolioList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './portfolioList.scss'
+const PortfolioList = ({id, title, active, setSelected}) => {
+  return (
+    <li className={active ? "PortfolioList active" : "PortfolioList"} onClick={()=>setSelected(id)}>
+    {title}
+    </li>
+  )
+}
+
+export default PortfolioList;
